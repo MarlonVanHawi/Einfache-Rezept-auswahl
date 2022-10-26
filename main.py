f99a78d542b4dcfddde612aa66e12ec1311866ab
MarlonVanHawi <111571018+MarlonVanHawi@users.noreply.github.com>
2022-10-26 14:54:02 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+auswahl= input("Rezept auswählen")
+
+if auswahl == str(1):
+    print("Pfannkuchen Rezept")
+if auswahl == str(2):
+    print("Waffel Rezept")
+if auswahl == str(3):
+    print("Käsekuchen Rezept")
+
